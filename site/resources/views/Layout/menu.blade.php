38acfb3206c0226cc915bac6c3a496eb9634bbb4
Tirtho76 <63685603+Tirtho76@users.noreply.github.com>
2021-01-23 12:59:26 +0600

    commit

---
+++ site/resources/views/Layout/menu.blade.php
@@ -0,0 +1,26 @@
+<!-- This is navbar part -->
+<nav class="navbar fixed-top nav-before navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand"><img class="nav-logo" src="images/classroom1.jpg"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav mr-auto mt-3 mt-lg-0">
+            <li class="nav-item active">
+                <a class="nav-link nav-font font-weight-bold" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link nav-font font-weight-bold" href="#">Course</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link nav-font font-weight-bold" href="#">Project</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link nav-font font-weight-bold" href="#">Blog</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link nav-font font-weight-bold" href="#">Contact</a>
+            </li>
+        </ul>        
+    </div>
+</nav>

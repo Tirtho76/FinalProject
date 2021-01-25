67147c29d4a94853161466d90569e8c35f44549b
Tirtho76 <63685603+Tirtho76@users.noreply.github.com>
2021-01-25 11:59:56 +0600

    commit

--- admin/public/js/custom.js
+++ admin/public/js/custom.js
@@ -2,3 +2,52 @@ $(document).ready(function () {
 $('#VisitorDt').DataTable();
 $('.dataTables_length').addClass('bs-select');
 });
+
+//For Course Table 
+function getCourseData() {
+    axios.get('/getCourseData')
+
+        .then(function(response) {
+
+            if (response.status == 200) {
+
+                $('#mainDivCourse').removeClass('d-none');
+                $('#loaderDivCourse').addClass('d-none');
+
+               
+                $('#courseDataTable').DataTable().destroy();
+                $('#course_table').empty();
+
+                
+                var jsonData = response.data;
+
+                $.each(jsonData, function(i, item) {
+                    $('<tr>').html(
+                        "<td>" + jsonData[i].course_name + "</td>" +
+                        "<td>" + jsonData[i].course_des  + "</td>" +
+
+                        "<td><a  class='courseViewDetailsBtn' data-id=" + jsonData[i].id + "><i class='fas fa-eye'></i></a></td>" +
+
+                        "<td><a  class='courseEditBtn' data-id=" + jsonData[i].id + "><i class='fas fa-edit'></i></a></td>" +
+
+                        "<td><a  class='courseDeleteBtn'  data-id=" + jsonData[i].id +" ><i class='fas fa-trash-alt'></i></a></td>"
+
+                    ).appendTo('#course_table');
+                });
+
+                // $('#serviceDataTable').DataTable({"order":false});
+                // $('.dataTables_length').addClass('bs-select');               
+
+            } else {
+                $('#loaderDivCourse').addClass('d-none');
+                $('#WrongDivCourse').removeClass('d-none');
+            }
+
+        })
+        .catch(function(error) {
+            $('#loaderDivCourse').addClass('d-none');
+            $('#WrongDivCourse').removeClass('d-none');
+        });
+
+}
+
--- admin/resources/views/Course.blade.php
+++ admin/resources/views/Course.blade.php
@@ -1,6 +1,6 @@
 @extends('Layout.app')
 @section('content')
-<div class="container">
+<div id="mainDivCourse" class="container d-none">
     <div class="row">
         <div class="col-md-12 p-5">
             <table id="" class="table table-striped table-bordered" cellspacing="0" width="100%">
@@ -13,16 +13,32 @@
                         <th class="th-sm">Delete</th>
                     </tr>
                 </thead>
-                <tbody>                    
+                <tbody id="course_table">                    
                     <tr>
-                        <td class="th-sm">Laravel</td>
-                        <td class="th-sm">Laravel is a fronend framework</td>
-                        <td class="th-sm"><a href="" class="btn btn-success">Details</a></td>
-                        <td class="th-sm"><a href="" class="btn btn-warning">Edit</a></td>
-                        <td class="th-sm"><a href="" class="btn btn-danger">Delete</a></td>
+                        <!-- <td class="th-sm">Laravel</td>
+                        <td class="th-sm">Laravel is a fronend framework<td>                        
+                        <td class="th-sm"><a href="" ><i class="fas fa-eye"></i></a></td>
+                        <td class="th-sm"><a href="" ><i class="fas fa-edit"></i></a></td>
+	                    <td class="th-sm"><a href="" ><i class="fas fa-trash-alt"></i></a></td> -->
                     </tr>                                                    
                 </tbody>
             </table>
+        </div>
+    </div>
+</div>
+
+<div id="loaderDivCourse" class="container">
+    <div class="row">
+        <div class="col-md-12 text-center p-5">
+            <img class="loading-icon m-5" src="{{asset('images/loader.svg')}}">
+        </div>
+    </div>
+</div>
+
+<div id="WrongDivCourse" class="container d-none">
+    <div class="row">
+        <div class="col-md-12 text-center p-5">
+            <h3>Something Went Wrong !</h3>
         </div>
     </div>
 </div>
@@ -32,6 +48,6 @@
 
 @section('script')
 <script type="text/javascript">
-    
+    getCourseData();
 </script>
 @endsection
